381f7bfb3b16f7315854e14f91be5752b62c86da
Josh Ford <thisisjoshford@gmail.com>
2020-03-22 14:49:28 -0700

    _blank added to fund detail links

--- src/FundDetail.js
+++ src/FundDetail.js
@@ -17,14 +17,26 @@ export default class FundDetail extends Component {
     //just so we don't have to type this.state.fund for each field : )
     const { fund } = this.state;
     // a turnary was used for the assistance link as not all of them have one... if there isn't one present it just returns an empty string... tried an if statement but couldn't get it to work... maybe one of you can come up with a better solution : ) also needs some CSS love
+    //implemented _blank to the links that will open a new tab... then it yelled at me that I need to use the rel="noreferrer noopener" for security so I did : )
     return (
           <div className='fund-detail'>
               <h3>{fund.name_of_fund}</h3>
               <h4>Administered by {fund.administrator}</h4>
               <p>{fund.purpose}</p>
-              <a href={fund.donate_link}>Donate Here</a><br/>
+              <a 
+                href={fund.donate_link}
+                target="_blank"
+                rel="noreferrer noopener"
+                > 
+                Donate Here
+              </a>
+              <br/>
               { fund.assistance_link ?
-                <a href={fund.assistance_link}>
+              <a 
+                href={fund.assistance_link}
+                target="_blank"
+                rel="noreferrer noopener"
+                >
                   Apply for Aid
                 </a>
                 :''
