9764762dc18a9f367525fb43a5df29c90ad4fa3e
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-25 22:47:27 -0700

    more styling for splash page

--- src/Splash.js
+++ src/Splash.js
@@ -4,18 +4,26 @@ export default class Splash extends Component {
   render() {
     return (
       <div class="splash-body">
-        <p>The coronavirus pandemic is creating unprecedented medical and economic challenges.  In addition to exposing long-standing gaps in our public health infrastructure, the virus and the associated pandemic mitigation measures are straining the financial resources of individuals, communities, and local governments to the breaking point.</p>
-        <p>We need to do what we can to #FlattenTheCurve, including complying with public restrictions on non-essential activities and working remotely. But as we know, pandemic mitigation measures hit some people harder than others. <a href="https://www.theatlantic.com/politics/archive/2020/03/covid-19s-devastating-effects-jobs-and-businesses/608461/">Economic experts expect job losses in the millions.</a> Other workers are unable to work from home, either because they are essential to fighting the pandemic or because their employers do not provide paid leave. These workers are at increased risk of contracting COVID-19 and incurring significant medical bills.</p>
+        <div class="splash-intro">
+          <p>The coronavirus pandemic is creating unprecedented medical and economic challenges.  In addition to exposing long-standing gaps in our public health infrastructure, the virus and the associated pandemic mitigation measures are straining the financial resources of individuals, communities, and local governments to the breaking point.</p>
+          <p>We need to do what we can to #FlattenTheCurve, including complying with public restrictions on non-essential activities and working remotely. But as we know, pandemic mitigation measures hit some people harder than others. <a href="https://www.theatlantic.com/politics/archive/2020/03/covid-19s-devastating-effects-jobs-and-businesses/608461/">Economic experts expect job losses in the millions.</a> Other workers are unable to work from home, either because they are essential to fighting the pandemic or because their employers do not provide paid leave. These workers are at increased risk of contracting COVID-19 and incurring significant medical bills.</p>
+        </div>
+        <div class="splash-section">
         <h3>Using This Site</h3>
-        <p>The COVID-19 Aid Funds site is designed to help donors with a little extra income support groups or sectors likely to suffer significant financial consequences because of the COVID-19 pandemic and its necessary mitigation measures. <b>This is a crowd-sourced list. The compilers are not affiliated with any of the funds, do not benefit from any of them, and have not personally vetted them. Please continue to use your own judgment and critical thinking skills.</b></p>
-        <p>If you have extra income right now (maybe from coffees you're not buying, bars you're not visiting, concerts and festivals you're not attending), consider donating to one or more of these funds. Every donation opportunity on our list is a way you can help while sitting on your couch at home.  You can filter the list by geographic location and type of beneficiary if you want to help a specific community.</p>
+          <p>The COVID-19 Aid Funds site is designed to help donors with a little extra income support groups or sectors likely to suffer significant financial consequences because of the COVID-19 pandemic and its necessary mitigation measures. <b>This is a crowd-sourced list. The compilers are not affiliated with any of the funds, do not benefit from any of them, and have not personally vetted them. Please continue to use your own judgment and critical thinking skills.</b></p>
+          <p>If you have extra income right now (maybe from coffees you're not buying, bars you're not visiting, concerts and festivals you're not attending), consider donating to one or more of these funds. Every donation opportunity on our list is a way you can help while sitting on your couch at home.  You can filter the list by geographic location and type of beneficiary if you want to help a specific community.</p>
+        </div>
+        <div class="splash-section">
         <h3>Help Us Expand Our List</h3>
-        <p>We are constantly updating the list.  If you know of a donation opportunity that is not on our list, please let us know on our Submissions page.</p>
-        <p>The Submissions page is also where you should go if you have direct information that one of the funds on our list is no longer accepting donations, or is operating fraudulently.</p>
-        <h3>Other Ways To Help</h3>
-        <p>Communities of all kinds are coming together to support each other during these difficult times.  This site is about how donate money, but we're also constantly updating our Resources page, which has other ways to help.</p>
-        <p>If you want to join a mutual aid project in your community, we recommend <a href="https://www.mutualaidhub.org/">this excellent resource.</a></p>
-        <p>Our Resources page includes links to other lists compiled by people across the country, including some really great resources for specific communities.  If you are the administrator of such a list and you want us include it in our Resources page, or you would like your spreadsheet integrated into our comprehensive list, please email us at: covid19aidfunds [at] gmail [dot] com.</p>
+          <p>We are constantly updating the list.  If you know of a donation opportunity that is not on our list, please let us know on our Submissions page.</p>
+          <p>The Submissions page is also where you should go if you have direct information that one of the funds on our list is no longer accepting donations, or is operating fraudulently.</p>
+        </div>
+        <div class="splash-section">
+          <h3>Other Ways To Help</h3>
+          <p>Communities of all kinds are coming together to support each other during these difficult times.  This site is about how donate money, but we're also constantly updating our Resources page, which has other ways to help.</p>
+          <p>If you want to join a mutual aid project in your community, we recommend <a href="https://www.mutualaidhub.org/">this excellent resource.</a></p>
+          <p>Our Resources page includes links to other lists compiled by people across the country, including some really great resources for specific communities.  If you are the administrator of such a list and you want us include it in our Resources page, or you would like your spreadsheet integrated into our comprehensive list, please email us at: covid19aidfunds [at] gmail [dot] com.</p>
+        </div>
       </div>
     )
   }
