d01c64afabb5b38970416993774dc1d7d036914f
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-29 00:31:30 -0700

    fix resources padding

--- src/Resources.js
+++ src/Resources.js
@@ -11,39 +11,39 @@ export default class Resources extends Component {
                 <main>
                     <ul>
                         <li> 
-                            <h5>Boston COVID-19 Community Care</h5>
+                            <h3>Boston COVID-19 Community Care</h3>
                             <a href="https://docs.google.com/spreadsheets/d/15GYuPYEzBk9KIyH3C3419aYxIMVAsa7BL7nBl9434Mg/edit?fbclid=IwAR05QVac9IgDQGHkZAFhapBD0r07Y-NzHE225J1u07ibXa8979oUdfagfk4#gid=0" target="_blank" rel="noopener noreferrer">COVID-19 resources for the Boston area.</a>
                         </li> 
                         <li> 
-                            <h5>DC Virtual Tip Jar</h5>
+                            <h3>DC Virtual Tip Jar</h3>
                             <a href="https://docs.google.com/spreadsheets/d/1tz2uyhgy3MsBS68MHPzO8H455_879fqfIaRPyUUw3QE/htmlview?sle=true">List of individuals in the food and beverage industry affected by pandemic mitigation measures, with contact for direct donations.</a>
                         </li> 
                         <li> 
-                            <h5>Coronavirus 2020 Artist Relief Funds</h5>
+                            <h3>Coronavirus 2020 Artist Relief Funds</h3>
                             <a href="https://docs.google.com/spreadsheets/d/1fFyKzxtzSFGzzvpRySJNtsVbjJhwnK7-N9KJxSHrW6w/edit#gid=0">List of resources for creators and artists dealing with cancelled gigs due to COVID-19.  Detailed information about each resource and application process.</a>
                         </li> 
                         <li> 
-                            <h5>Black Girl Majik COVID-19 Resource Guide</h5>
+                            <h3>Black Girl Majik COVID-19 Resource Guide</h3>
                             <a href="https://docs.google.com/spreadsheets/d/1wevZCAYAWylQYhuYynaai_qBwIjPgK-9umo5eRP2Cpg/edit#gid=1389152448">List of resources ranging from self-care to mutual aid to donation opportunities, organized by category.</a>
                         </li> 
                         <li> 
-                            <h5>COVID-19 Financial Solidarity</h5>
+                            <h3>COVID-19 Financial Solidarity</h3>
                             <a href="https://docs.google.com/spreadsheets/u/1/d/1FBEU0cdAGpScU0JrOlYFm8FrY1Bjr0_HsyW4hWOj74I/htmlview?fbclid=IwAR0wyJvUh2lW1AaY9IpVsne3RCRHtrDuCO0cJZKfneRXWtWchP51SjA9KLM&sle=true#gid=0">Self-identified list of individuals affected by pandemic mitigation measures, with information for direct contribution.</a>
                         </li> 
                         <li> 
-                            <h5>Freelancers or Community Resources</h5>
+                            <h3>Freelancers or Community Resources</h3>
                             <a href="https://docs.google.com/document/d/1xt1QZlGsyga_HrXagubV9O0rebV5dx4DuMOd2sWvWwc/edit#">Comprehensive list of resources for freelancers and creatives impacted by gigs being cancelled/postponed.</a>
                         </li> 
                         <li> 
-                            <h5>COVID-19 Los Angeles Mass Resource List</h5>
+                            <h3>COVID-19 Los Angeles Mass Resource List</h3>
                             <a href="https://docs.google.com/document/u/0/d/1S-WJaMa4q3yNrEBfSFKEdSQArcNc_MTI2LiUA63Ycyg/mobilebasic">Comprehensive list of resources for people and communities affected by COVID-19 in the greater Los Angeles area.</a>
                         </li> 
                         <li> 
-                            <h5>COVID-19 Resources for Undocumented Communities</h5>
+                            <h3>COVID-19 Resources for Undocumented Communities</h3>
                             <a href="https://docs.google.com/spreadsheets/d/18p9OSlLpSYanIoUC-gEbhVbRMYVUfw4wyrixa9ekGdc/edit#gid=0">Comprehensive list of resources for undocumented individuals and communities affected by COVID-19.  Nationwide resources and state-by-state.</a>
                         </li> 
                         <li> 
-                            <h5>PDX COVID-19 Resource list</h5>
+                            <h3>PDX COVID-19 Resource list</h3>
                             <a href="https://docs.google.com/spreadsheets/d/1DKH9m36nzZuar563Ier1xPVIGY6Bb3kEDv1c2RcGgqM/htmlview">Comprehensive list of resources for people and communities affected by COVID-19 in the greater Portland area.</a>
                         </li> 
                     </ul>
